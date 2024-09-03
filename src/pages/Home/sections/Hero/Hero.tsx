85fa17742602c2daa8fedd6633d218e405694e50
Matheus dos Santos Soares <maverick17xd@gmail.com>
2024-09-03 14:19:05 -0300

    fix: Hero component UI

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -11,10 +11,18 @@ const Hero = () => {
     height: "100vh",
     display: "flex",
     alignItems: "center",
+    [theme.breakpoints.up("xs")]: {
+      // <=mobile
+      paddingTop: "100px",
+    },
+    [theme.breakpoints.up("md")]: {
+      // >mobile
+      paddingTop: 0,
+    },
   }));
 
   const StyledImg = styled("img")(({ theme }) => ({
-    width: "80%",
+    width: "75%",
     borderRadius: "50%",
     border: `1px solid ${theme.palette.primary.contrastText}`,
   }));
@@ -26,11 +34,12 @@ const Hero = () => {
           <Grid container spacing={2}>
             <Grid item xs={12} md={5}>
               <Box position="relative">
-                <Box position="absolute" width={"150%"} top={-100} right={0} >
+                <Box position="absolute" width={"150%"} top={-100} right={0}>
                   <AnimatedBackground />
                 </Box>
-                <Box position="relative" textAlign="center"><StyledImg src={Avatar} /></Box>
-               
+                <Box position="relative" textAlign="center">
+                  <StyledImg src={Avatar} />
+                </Box>
               </Box>
             </Grid>
             <Grid item xs={12} md={7}>
