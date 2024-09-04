86df0e3ab465f9d0ed542a3599a54823e6b205d6
Matheus dos Santos Soares <maverick17xd@gmail.com>
2024-09-04 14:56:54 -0300

    feat: changes Navbar style for mobile devices

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,27 +1,119 @@
-import { AppBar, MenuItem, styled, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  IconButton,
+  MenuItem,
+  styled,
+  Toolbar,
+  Drawer,
+  List,
+  ListItem,
+  Box,
+  ListItemText,
+  Divider,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+import theme from "../../theme";
 
 const NavBar = () => {
-  const StyledToolbar = styled(Toolbar)(({ }) => ({
-   display: 'flex',
-   justifyContent: 'space-evenly'
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "space-evenly",
+    [theme.breakpoints.down("md")]: {
+      justifyContent: "space-between",
+    },
   }));
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
   const handleScroll = (sectionId: string) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
+    if (drawerOpen) {
+      setDrawerOpen(false);
+    }
   };
 
   return (
     <>
       <AppBar position="fixed">
-        <StyledToolbar>  
-          <MenuItem onClick={() => handleScroll("about-section")}>About</MenuItem>
-          <MenuItem>Skills</MenuItem>
-          <MenuItem>Projects</MenuItem>
+        <StyledToolbar>
+          <Box
+            sx={{
+              display: { xs: "none", md: "flex" },
+              flexGrow: 1,
+              justifyContent: "space-evenly",
+            }}
+          >
+            <MenuItem onClick={() => handleScroll("about-section")}>
+              About
+            </MenuItem>
+            <MenuItem onClick={() => handleScroll("skills-section")}>
+              Skills
+            </MenuItem>
+            <MenuItem onClick={() => handleScroll("projects-section")}>
+              Projects
+            </MenuItem>
+          </Box>
+          <IconButton
+            sx={{ display: { xs: "block", md: "none" } }}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerToggle}
+          >
+            <MenuIcon />
+          </IconButton>
         </StyledToolbar>
       </AppBar>
+
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+        sx={{
+          ".MuiDrawer-paper": {
+            width: "10rem",
+            height: "auto",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: theme.palette.primary.contrastText,
+            backgroundColor: theme.palette.primary.main,
+           
+          },
+        }}
+      >
+        <List sx={{ width: "100%", paddingLeft: "10px", paddingRight: "10px" }}>
+          <ListItem component="a" onClick={() => handleScroll("about-section")}>
+            <ListItemText primary="About" />
+          </ListItem>
+          <Divider
+            sx={{ backgroundColor: theme.palette.primary.contrastText }}
+          />
+          <ListItem
+            component="a"
+            onClick={() => handleScroll("skills-section")}
+          >
+            <ListItemText primary="Skills" />
+          </ListItem>
+          <Divider
+            sx={{ backgroundColor: theme.palette.primary.contrastText}}
+          />
+          <ListItem
+            component="a"
+            onClick={() => handleScroll("projects-section")}
+          >
+            <ListItemText primary="Projects" />
+          </ListItem>
+        </List>
+      </Drawer>
     </>
   );
 };
