7ceefbbdc93b774b917fb0cbda1681321ee4c263
Matheus dos Santos Soares <maverick17xd@gmail.com>
2024-09-10 13:39:09 -0300

    feat: inits InfoBox component in About Me

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -1,44 +1,59 @@
-import { Container, Typography, Box, styled } from "@mui/material";
+import { Container, Typography, Box, styled, Grid } from "@mui/material";
+import InfoBox from "./InfoBox";
+import SchoolIcon from "@mui/icons-material/School";
 
 const About = () => {
-
-    const StyledAbout = styled("div")(({ theme }) => ({
-        backgroundColor: theme.palette.primary.contrastText,
-        height: "50vh",
-        display: "flex",
-        alignItems: "center",
-        [theme.breakpoints.up("xs")]: {
-          // <=mobile
-          paddingTop: "100px",
-        },
-        [theme.breakpoints.up("md")]: {
-          // >mobile
-          paddingTop: 0,
-        },
-      }));
-
+  const StyledAbout = styled("div")(({ theme }) => ({
+    backgroundColor: theme.palette.primary.contrastText,
+    minHeight: "50vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: theme.spacing(4, 2),
+    [theme.breakpoints.up("md")]: {
+      // >mobile
+      paddingTop: 0,
+      padding: theme.spacing(8, 4),
+    },
+  }));
 
   return (
-    <> 
-    <StyledAbout>
-    <Container maxWidth="lg" sx={{ py: 4 }}>
-      <Box sx={{ textAlign: "center" }}>
-        <Typography variant="h2" color="primary" gutterBottom>
-          About Me
-        </Typography>
-        <Typography variant="body1" color="textPrimary" sx={{ mt: 2 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-          lacinia odio vitae vestibulum. Donec et venenatis dolor. Integer
-          imperdiet, purus vel ultricies auctor, augue lacus elementum elit, et
-          cursus lacus leo at purus.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-          lacinia odio vitae vestibulum. Donec et venenatis dolor. Integer
-          imperdiet, purus vel ultricies auctor, augue lacus elementum elit, et
-          cursus lacus leo at purus.
-        </Typography>
-      </Box>
-    </Container>
-    </StyledAbout>
+    <>
+      <StyledAbout>
+        <Container maxWidth="lg">
+          <Box sx={{ textAlign: "center" }}>
+            <Typography variant="h2" color="primary" gutterBottom>
+              About Me
+            </Typography>
+            <Grid container spacing={4} sx={{ mt: 4 }}>
+              <Grid item xs={12} md={6}>
+                <InfoBox
+                  icon={SchoolIcon}
+                  title="Education"
+                  description={"1+ years Front-end Development"}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <InfoBox
+                  icon={SchoolIcon}
+                  title="Education"
+                  description="Bachelor's Degree in Electrical Engineering"
+                />
+              </Grid>
+            </Grid>
+            <Typography variant="body1" color="textPrimary" sx={{ mt: 2 }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              lacinia odio vitae vestibulum. Donec et venenatis dolor. Integer
+              imperdiet, purus vel ultricies auctor, augue lacus elementum elit,
+              et cursus lacus leo at purus. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Vivamus lacinia odio vitae
+              vestibulum. Donec et venenatis dolor. Integer imperdiet, purus vel
+              ultricies auctor, augue lacus elementum elit, et cursus lacus leo
+              at purus.
+            </Typography>
+          </Box>
+        </Container>
+      </StyledAbout>
     </>
   );
 };
