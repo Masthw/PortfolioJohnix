c19f427662f3add6f8d3ce0277c27ba6c8fca528
Matheus dos Santos Soares <maverick17xd@gmail.com>
2024-09-12 14:52:19 -0300

    feat: animates SkillCards in Skill section

--- src/pages/Home/sections/Skills/SkillCard.tsx
+++ src/pages/Home/sections/Skills/SkillCard.tsx
@@ -1,30 +1,50 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, keyframes } from "@mui/material";
+import { forwardRef } from "react";
 import theme from "../../../../theme";
 
 interface SkillCardProps {
   title: string;
-  description?: string;
+  delay?: number;
+  animationClass?: string;
 }
 
-const SkillCard: React.FC<SkillCardProps> = ({ title }) => (
-  <Box
-  display="flex"
-        flexDirection="column"
-        alignItems="center"
-        p={2}
-        borderRadius="8px"
-        boxShadow="0 4px 8px rgba(0,0,0,0.1)"
-        sx={{
-          backgroundColor: "background.paper",
-          transition: "background-color 0.3s ease",
-          "&:hover": {
-            backgroundColor: theme.palette.secondary.light,
-          },
-        }}>
-    <Typography variant="body1" gutterBottom>
-      {title}
-    </Typography>
-  </Box>
+
+const growAndShrink = keyframes`
+  0% {
+    transform: scale(0.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+// Usando forwardRef para permitir o uso de ref externa
+const SkillCard = forwardRef<HTMLDivElement, SkillCardProps>(
+  ({ title, delay = 0, animationClass = "" }, ref) => (
+    <Box
+      ref={ref} 
+      className={animationClass} 
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      p={2}
+      borderRadius="8px"
+      boxShadow="0 4px 8px rgba(0,0,0,0.1)"
+      sx={{
+        backgroundColor: "background.paper",
+        animation: `${growAndShrink} 0.8s ease ${delay}s forwards`,
+        transform: "scale(0.5)",
+        transition: "background-color 0.3s ease",
+        "&:hover": {
+          backgroundColor: theme.palette.secondary.light,
+        },
+      }}
+    >
+      <Typography variant="body1" gutterBottom>
+        {title}
+      </Typography>
+    </Box>
+  )
 );
 
 export default SkillCard;
--- src/pages/Home/sections/Skills/Skills.tsx
+++ src/pages/Home/sections/Skills/Skills.tsx
@@ -1,7 +1,19 @@
 import { Container, Typography, Box, Grid, styled } from "@mui/material";
 import SkillCard from "./SkillCard";
+import { useEffect, useRef, useState } from "react";
+
 
 const Skills = () => {
+  const [isVisible, setIsVisible] = useState({
+    javascript: false,
+    react: false,
+    flutter: false,
+  });
+
+  const javascriptRef = useRef<HTMLDivElement | null>(null);
+  const reactRef = useRef<HTMLDivElement | null>(null);
+  const flutterRef = useRef<HTMLDivElement | null>(null);
+
   const StyledSkills = styled("div")(({ theme }) => ({
     backgroundColor: theme.palette.background.default,
     minHeight: "30vh",
@@ -14,6 +26,37 @@ const Skills = () => {
     },
   }));
 
+  useEffect(() => {
+    const observerCallback = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.target === javascriptRef.current && entry.isIntersecting) {
+          setIsVisible((prev) => ({ ...prev, javascript: true }));
+        }
+        if (entry.target === reactRef.current && entry.isIntersecting) {
+          setIsVisible((prev) => ({ ...prev, react: true }));
+        }
+        if (entry.target === flutterRef.current && entry.isIntersecting) {
+          setIsVisible((prev) => ({ ...prev, flutter: true }));
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, {
+      root: null,
+      threshold: 0.5, // Só dispara a animação quando 50% da skill estiver visível
+    });
+
+    if (javascriptRef.current) observer.observe(javascriptRef.current);
+    if (reactRef.current) observer.observe(reactRef.current);
+    if (flutterRef.current) observer.observe(flutterRef.current);
+
+    return () => {
+      if (javascriptRef.current) observer.unobserve(javascriptRef.current);
+      if (reactRef.current) observer.unobserve(reactRef.current);
+      if (flutterRef.current) observer.unobserve(flutterRef.current);
+    };
+  }, []);
+
   return (
     <StyledSkills>
       <Container maxWidth="lg">
@@ -23,13 +66,28 @@ const Skills = () => {
           </Typography>
           <Grid container spacing={4}>
             <Grid item xs={12} md={4}>
-              <SkillCard title="JavaScript" />
+              <SkillCard
+                title="JavaScript"
+                delay={0.1}
+                ref={javascriptRef}
+                animationClass={isVisible.javascript ? "grow-animation" : ""}
+              />
             </Grid>
             <Grid item xs={12} md={4}>
-              <SkillCard title="React" />
+              <SkillCard
+                title="React"
+                delay={0.3}
+                ref={reactRef}
+                animationClass={isVisible.react ? "grow-animation" : ""}
+              />
             </Grid>
             <Grid item xs={12} md={4}>
-              <SkillCard title="Flutter" />
+              <SkillCard
+                title="Flutter"
+                delay={0.5}
+                ref={flutterRef}
+                animationClass={isVisible.flutter ? "grow-animation" : ""}
+              />
             </Grid>
           </Grid>
         </Box>
