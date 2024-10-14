285fe181361dddd1310e0aef7eef4b817218e389
Matheus dos Santos Soares <maverick17xd@gmail.com>
2024-10-14 19:34:54 -0300

    feat: add more photos to projects section

--- src/pages/Home/sections/Projects/ProjectDetail.tsx
+++ src/pages/Home/sections/Projects/ProjectDetail.tsx
@@ -8,17 +8,46 @@ import Foto1P1 from "../../../../assets/images/projeto1/Foto1.jpg";
 import Foto2P1 from "../../../../assets/images/projeto1/Foto2.jpg";
 import Foto3P1 from "../../../../assets/images/projeto1/Foto3.jpg";
 import Foto1P2 from "../../../../assets/images/projeto2/Foto1.jpg";
-import Foto2P2 from "../../../../assets/images/projeto2/Foto2.jpg";
 import Foto1P3 from "../../../../assets/images/projeto3/Foto1.jpg";
 import Foto2P3 from "../../../../assets/images/projeto3/Foto2.jpg";
 import Foto3P3 from "../../../../assets/images/projeto3/Foto3.jpg";
 import Foto4P3 from "../../../../assets/images/projeto3/Foto4.jpg";
-
+import Foto1P4 from "../../../../assets/images/projeto4/Foto1.jpg";
+import Foto2P4 from "../../../../assets/images/projeto4/Foto2.jpg";
+import Foto3P4 from "../../../../assets/images/projeto4/Foto3.jpg";
+import Foto1P5 from "../../../../assets/images/projeto5/Foto1.jpg";
+import Foto1P6 from "../../../../assets/images/projeto6/Foto1.jpg";
+import Foto2P6 from "../../../../assets/images/projeto6/Foto2.jpg";
+import Foto3P6 from "../../../../assets/images/projeto6/Foto3.jpg";
+import Foto4P6 from "../../../../assets/images/projeto6/Foto4.jpg";
+import Foto5P6 from "../../../../assets/images/projeto6/Foto5.jpg";
+import Foto1P7 from "../../../../assets/images/projeto7/Foto1.jpg";
+import Foto2P7 from "../../../../assets/images/projeto7/Foto2.jpg";
+import Foto3P7 from "../../../../assets/images/projeto7/Foto3.jpg";
+import Foto4P7 from "../../../../assets/images/projeto7/Foto4.jpg";
+import Foto5P7 from "../../../../assets/images/projeto7/Foto5.jpg";
+import Foto6P7 from "../../../../assets/images/projeto7/Foto6.jpg";
+import Foto7P7 from "../../../../assets/images/projeto7/Foto7.jpg";
+import Foto1P8 from "../../../../assets/images/projeto8/Foto1.jpg";
+import Foto2P8 from "../../../../assets/images/projeto8/Foto2.jpg";
+import Foto3P8 from "../../../../assets/images/projeto8/Foto3.jpg";
+import Foto4P8 from "../../../../assets/images/projeto8/Foto4.jpg";
+import Foto5P8 from "../../../../assets/images/projeto8/Foto5.jpg";
+import Foto6P8 from "../../../../assets/images/projeto8/Foto6.jpg";
+import Foto7P8 from "../../../../assets/images/projeto8/Foto7.jpg";
+import Foto8P8 from "../../../../assets/images/projeto8/Foto8.jpg";
+import Foto9P8 from "../../../../assets/images/projeto8/Foto9.jpg";
 
 const projectImages: { [key: string]: string[]} = {
   "1": [Foto1P1, Foto2P1, Foto3P1],
-  "2": [Foto1P2, Foto2P2],
+  "2": [Foto1P2],
   "3": [Foto1P3, Foto2P3, Foto3P3, Foto4P3],
+  "4": [Foto1P4, Foto2P4, Foto3P4],
+  "5": [Foto1P5],
+  "6": [Foto1P6, Foto2P6, Foto3P6, Foto4P6, Foto5P6],
+  "7": [Foto1P7, Foto2P7, Foto3P7, Foto4P7, Foto5P7, Foto6P7, Foto7P7],
+  "8": [Foto1P8, Foto2P8, Foto3P8, Foto4P8, Foto5P8, Foto6P8, Foto7P8, Foto8P8, Foto9P8],
+  
 }
 
 const StyledProjects = styled("div")(({ theme }) => ({
@@ -76,17 +105,25 @@ const ProjectDetail = () => {
         <Grid container spacing={4} justifyContent="center">
           <Grid item xs={12}>
             <Box sx={{ position: "relative" }}>
-              <Slider {...settings}>
-                {images.map((image, index) => (
-                  <div key={index}>
-                    <img
-                      src={image}
-                      alt={`Project ${projectId} - Image ${index + 1}`}
-                      style={{ width: "100%", height: "auto", maxHeight: "80vh", objectFit: "contain" }}
-                    />
-                  </div>
-                ))}
-              </Slider>
+            {projectId === "2" || projectId === "5" ? (
+                <img
+                  src={images[0]}
+                  alt={`Project ${projectId} - Image 1`}
+                  style={{ width: "100%", height: "auto", maxHeight: "80vh", objectFit: "contain" }}
+                />
+              ) : (
+                <Slider {...settings}>
+                  {images.map((image, index) => (
+                    <div key={index}>
+                      <img
+                        src={image}
+                        alt={`Project ${projectId} - Image ${index + 1}`}
+                        style={{ width: "100%", height: "auto", maxHeight: "80vh", objectFit: "contain" }}
+                      />
+                    </div>
+                  ))}
+                </Slider>
+              )}
             </Box>
             <IconButton
                 onClick={() => navigate(-1)}
--- src/pages/Home/sections/Projects/Projects.tsx
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -19,11 +19,35 @@ import Foto1P1 from "../../../../assets/images/projeto1/Foto1.jpg";
 import Foto2P1 from "../../../../assets/images/projeto1/Foto2.jpg";
 import Foto3P1 from "../../../../assets/images/projeto1/Foto3.jpg";
 import Foto1P2 from "../../../../assets/images/projeto2/Foto1.jpg";
-import Foto2P2 from "../../../../assets/images/projeto2/Foto2.jpg";
 import Foto1P3 from "../../../../assets/images/projeto3/Foto1.jpg";
 import Foto2P3 from "../../../../assets/images/projeto3/Foto2.jpg";
 import Foto3P3 from "../../../../assets/images/projeto3/Foto3.jpg";
 import Foto4P3 from "../../../../assets/images/projeto3/Foto4.jpg";
+import Foto1P4 from "../../../../assets/images/projeto4/Foto1.jpg";
+import Foto2P4 from "../../../../assets/images/projeto4/Foto2.jpg";
+import Foto3P4 from "../../../../assets/images/projeto4/Foto3.jpg";
+import Foto1P5 from "../../../../assets/images/projeto5/Foto1.jpg";
+import Foto1P6 from "../../../../assets/images/projeto6/Foto1.jpg";
+import Foto2P6 from "../../../../assets/images/projeto6/Foto2.jpg";
+import Foto3P6 from "../../../../assets/images/projeto6/Foto3.jpg";
+import Foto4P6 from "../../../../assets/images/projeto6/Foto4.jpg";
+import Foto5P6 from "../../../../assets/images/projeto6/Foto5.jpg";
+import Foto1P7 from "../../../../assets/images/projeto7/Foto1.jpg";
+import Foto2P7 from "../../../../assets/images/projeto7/Foto2.jpg";
+import Foto3P7 from "../../../../assets/images/projeto7/Foto3.jpg";
+import Foto4P7 from "../../../../assets/images/projeto7/Foto4.jpg";
+import Foto5P7 from "../../../../assets/images/projeto7/Foto5.jpg";
+import Foto6P7 from "../../../../assets/images/projeto7/Foto6.jpg";
+import Foto7P7 from "../../../../assets/images/projeto7/Foto7.jpg";
+import Foto1P8 from "../../../../assets/images/projeto8/Foto1.jpg";
+import Foto2P8 from "../../../../assets/images/projeto8/Foto2.jpg";
+import Foto3P8 from "../../../../assets/images/projeto8/Foto3.jpg";
+import Foto4P8 from "../../../../assets/images/projeto8/Foto4.jpg";
+import Foto5P8 from "../../../../assets/images/projeto8/Foto5.jpg";
+import Foto6P8 from "../../../../assets/images/projeto8/Foto6.jpg";
+import Foto7P8 from "../../../../assets/images/projeto8/Foto7.jpg";
+import Foto8P8 from "../../../../assets/images/projeto8/Foto8.jpg";
+import Foto9P8 from "../../../../assets/images/projeto8/Foto9.jpg";
 import Slider from "react-slick";
 import CollectionsIcon from "@mui/icons-material/Collections";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
@@ -39,8 +63,13 @@ const Projects = () => {
 
  
   const imagesProject1 = [Foto1P1, Foto2P1, Foto3P1];
-  const imagesProject2 = [Foto1P2, Foto2P2];
+  const imagesProject2 = [Foto1P2];
   const imagesProject3 = [Foto1P3, Foto2P3, Foto3P3, Foto4P3];
+  const imagesProject4 = [Foto1P4, Foto2P4, Foto3P4];
+  const imagesProject5 = [Foto1P5];
+  const imagesProject6 = [Foto1P6, Foto2P6, Foto3P6, Foto4P6, Foto5P6];
+  const imagesProject7 = [Foto1P7, Foto2P7, Foto3P7, Foto4P7, Foto5P7, Foto6P7, Foto7P7];
+  const imagesProject8 = [Foto1P8, Foto2P8, Foto3P8, Foto4P8, Foto5P8, Foto6P8, Foto7P8, Foto8P8, Foto9P8];
 
   const project1Ref = useRef<HTMLDivElement | null>(null);
   const project2Ref = useRef<HTMLDivElement | null>(null);
@@ -50,6 +79,7 @@ const Projects = () => {
     backgroundColor: theme.palette.primary.main,
     padding: theme.spacing(8, 2),
     minHeight: "100vh",
+    paddingBottom: "2rem",
   }));
 
   useEffect(() => {
@@ -169,7 +199,7 @@ const Projects = () => {
           >
             Meu Trabalho
           </Typography>
-          <Grid container spacing={4}>
+          <Grid container spacing={4} alignItems="stretch">
             <Grid item xs={12} md={4}>
               <div
                 ref={project1Ref}
@@ -180,7 +210,7 @@ const Projects = () => {
                     position: "relative",
                     width: { xs: "100%", md: "350px" },
                     height: { xs: "auto", md: "350px" },
-                    marginBottom: "0.5rem",
+                    marginBottom: "10rem",
                   }}
                 >
                   <CollectionsIcon
@@ -225,36 +255,18 @@ const Projects = () => {
                     position: "relative",
                     width: { xs: "100%", md: "350px" },
                     height: { xs: "auto", md: "350px" },
+                    marginBottom: "10rem",
                   }}
                 >
-                  <CollectionsIcon
-                    sx={{
-                      position: "absolute",
-                      top: "10px",
-                      right: "10px",
-                      color: "white",
-                      fontSize: "1rem",
-                      zIndex: 1,
-                      opacity: isVisible.project1 ? 1 : 0,
-                    }}
-                  />
-                  <Slider {...settings}>
-                    {imagesProject2.map((image, index) => (
-                      <div
-                        key={index}
-                        onClick={() => handleProjectClick(2)}
-                      
-                      >
-                        <img
-                          src={image}
-                          alt={`Project 2 - Image ${index + 1}`}
-                          className={`${
-                            isVisible.project1 ? "popIn project-delay-2" : ""
-                          } carousel-image`}
-                        />
-                      </div>
-                    ))}
-                  </Slider>
+                 
+                 <img
+          src={imagesProject2[0]}
+          onClick={() => handleProjectClick(2)}
+          alt="Project 2 - Image"
+          className={`${
+            isVisible.project1 ? "popIn project-delay-2" : ""
+          } carousel-image`}
+        />
                 </Box>
               </div>
             </Grid>
@@ -270,6 +282,7 @@ const Projects = () => {
                     position: "relative",
                     width: { xs: "100%", md: "350px" },
                     height: { xs: "auto", md: "350px" },
+                    marginBottom: "10rem",
                   }}
                 >
                   <CollectionsIcon
@@ -304,6 +317,187 @@ const Projects = () => {
               </div>
             </Grid>
           </Grid>
+          <Grid container spacing={4} alignItems="stretch">
+          <Grid item xs={12} md={4}>
+            <div
+                className={
+                  isVisible.project1 ? "popIn project-delay-4" : "image"
+                }
+              >
+                <Box
+                  sx={{
+                    position: "relative",
+                    width: { xs: "100%", md: "350px" },
+                    height: { xs: "auto", md: "350px" },
+                    marginBottom: "10rem",
+                  }}
+                >
+                  <CollectionsIcon
+                    sx={{
+                      position: "absolute",
+                      top: "10px",
+                      right: "10px",
+                      color: "white",
+                      fontSize: "1rem",
+                      zIndex: 1,
+                      opacity: isVisible.project1 ? 1 : 0,
+                    }}
+                  />
+                  <Slider {...settings}>
+                    {imagesProject4.map((image, index) => (
+                      <div
+                        key={index}
+                        onClick={() => handleProjectClick(4)}
+                      
+                      >
+                        <img
+                          src={image}
+                          alt={`Project 4 - Image ${index + 1}`}
+                          className={`${
+                            isVisible.project1 ? "popIn project-delay-4" : ""
+                          } carousel-image`}
+                        />
+                      </div>
+                    ))}
+                  </Slider>
+                </Box>
+              </div> 
+            </Grid>
+            <Grid item xs={12} md={4}>
+    <div className={isVisible.project1 ? "popIn project-delay-5" : "image"}>
+      <Box
+        sx={{
+          position: "relative",
+          width: { xs: "100%", md: "350px" },
+          height: { xs: "auto", md: "350px" },
+          marginBottom: "10rem",
+        }}
+      >
+        <img
+          src={imagesProject5[0]}
+          onClick={() => handleProjectClick(5)}
+          alt="Project 2 - Image"
+          className={`${
+            isVisible.project1 ? "popIn project-delay-5" : ""
+          } carousel-image`}
+        />
+      </Box>
+    </div>
+  </Grid>
+  <Grid item xs={12} md={4}>
+    <div className={isVisible.project1 ? "popIn project-delay-6" : "image"}>
+      <Box
+        sx={{
+          position: "relative",
+          width: { xs: "100%", md: "350px" },
+          height: { xs: "auto", md: "350px" },
+          marginBottom: "10rem",
+        }}
+      >
+        <CollectionsIcon
+          sx={{
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            color: "white",
+            fontSize: "1rem",
+            zIndex: 1,
+            opacity: isVisible.project1 ? 1 : 0,
+          }}
+        />
+        <Slider {...settings}>
+          {imagesProject6.map((image, index) => (
+            <div key={index} onClick={() => handleProjectClick(6)}>
+              <img
+                src={image}
+                alt={`Project 6 - Image ${index + 1}`}
+                className={`${
+                  isVisible.project1 ? "popIn project-delay-6" : ""
+                } carousel-image`}
+              />
+            </div>
+          ))}
+        </Slider>
+      </Box>
+    </div>
+  </Grid>
+</Grid>
+<Grid container spacing={4} alignItems="stretch">
+<Grid item xs={12} md={4}>
+    <div className={isVisible.project1 ? "popIn project-delay-7" : "image"}>
+      <Box
+        sx={{
+          position: "relative",
+          width: { xs: "100%", md: "350px" },
+          height: { xs: "auto", md: "350px" },
+          marginBottom: "10rem",
+        }}
+      >
+        <CollectionsIcon
+          sx={{
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            color: "white",
+            fontSize: "1rem",
+            zIndex: 1,
+            opacity: isVisible.project1 ? 1 : 0,
+          }}
+        />
+        <Slider {...settings}>
+          {imagesProject7.map((image, index) => (
+            <div key={index} onClick={() => handleProjectClick(7)}>
+              <img
+                src={image}
+                alt={`Project 7 - Image ${index + 1}`}
+                className={`${
+                  isVisible.project1 ? "popIn project-delay-7" : ""
+                } carousel-image`}
+              />
+            </div>
+          ))}
+        </Slider>
+      </Box>
+    </div>
+  </Grid>
+<Grid item xs={12} md={4}>
+    <div className={isVisible.project1 ? "popIn project-delay-8" : "image"}>
+      <Box
+        sx={{
+          position: "relative",
+          width: { xs: "100%", md: "350px" },
+          height: { xs: "auto", md: "350px" },
+          marginBottom: "10rem",
+        }}
+      >
+        <CollectionsIcon
+          sx={{
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            color: "white",
+            fontSize: "1rem",
+            zIndex: 1,
+            opacity: isVisible.project1 ? 1 : 0,
+          }}
+        />
+        <Slider {...settings}>
+          {imagesProject8.map((image, index) => (
+            <div key={index} onClick={() => handleProjectClick(8)}>
+              <img
+                src={image}
+                alt={`Project 8 - Image ${index + 1}`}
+                className={`${
+                  isVisible.project1 ? "popIn project-delay-8" : ""
+                } carousel-image`}
+              />
+            </div>
+          ))}
+        </Slider>
+      </Box>
+    </div>
+  </Grid>
+  </Grid>
         </Box>
       </Container>
     </StyledProjects>
